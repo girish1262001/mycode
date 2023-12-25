5df56a1458cf5eccd5d0b0d8ff9e334b896af97b
girishpatil1262001 <girishpatil1262001@gmail.com>
2023-12-25 22:12:20 +0530

    new added angular

--- angularproject/myapllication/src/app/app.component.ts
+++ angularproject/myapllication/src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myapllication';
+  // proptyname : data type = value
+  a= 20;
+  b : number = 30;
+  c!: number
+  city: string = "Pune";
+  //1. one way db 2. two way db
+  //1. one way db
+  //1.string interpolation>>{{}} >>syntax {{propertyName}}
+  //2.property binding >>[]>> [attribute]="proprtyName"
+  //3. event binding>>()>>on event will bind data to the property
+  //Two way data bind >> [()]>> [(ngModel)]="propertyName"
+  myName = "Poonam";
+  isDisable = false;
+  fruit!:string;
+
+  constructor() { }
+
+  submit() {
+    this.fruit = "Mango";
+  }
 }
