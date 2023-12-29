748f3ca15d4a5e1ac0ff8272721e22376822f39e
girishpatil1262001 <girishpatil1262001@gmail.com>
2023-12-29 12:44:27 +0530

    new addeds

--- angularproject/myapllication/src/app/app.component.ts
+++ angularproject/myapllication/src/app/app.component.ts
@@ -27,4 +27,8 @@ export class AppComponent {
   submit() {
     this.fruit = "Mango";
   }
+  inputBoxValue(data:any){
+    console.log(data.target.value);
+    this.myName = data.target.value
+   } 
 }
